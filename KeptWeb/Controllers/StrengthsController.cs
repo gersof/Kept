302a1ca60afdf00ccf522f8e19be893fb6abc141
Nicolás Vallejo <nivato@msn.com>
2018-09-03 18:56:17 -0500

    added strengths functionality

--- KeptWeb/Controllers/StrengthsController.cs
+++ KeptWeb/Controllers/StrengthsController.cs
@@ -1,18 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using KeptWeb.Models;
+using KeptWeb.Repositories;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace KeptWeb.Controllers
 {
     public class StrengthsController : Controller
     {
-        // GET: Strenghts
+        private readonly KEPT_DBEntities _context;
+        private readonly StrengthsRepository _strengths;
+        private readonly EmployeeRepository _employee;
+
+        public StrengthsController()
+        {
+            _context = new KEPT_DBEntities();
+            _employee = new EmployeeRepository(_context);
+            _strengths = new StrengthsRepository(_context);
+        }
+
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var employee = _employee.GetEmployee(User.Identity.GetUserId());
+            var strengths = _strengths.GetStrenghtsbyEmployee(employee.DocumentId);
+            var strengthsByEmployee = new StrengthsByEmployee { Strengths = strengths };
+            return View(strengthsByEmployee);
         }
     }
 }
