f18b779d317b6997a571de8b886690ebfe436cad
William Ballesteros Blanco <ballesteroswilliam@live.com>
2018-09-03 10:56:54 -0500

    Kept add assessment competencies logic

--- KeptWeb/Controllers/AssessmentTestsController.cs
+++ KeptWeb/Controllers/AssessmentTestsController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KeptWeb.Models;
+using KeptWeb.Repositories;
 
 namespace KeptWeb.Controllers
 {
-    public class AssessmentTestsController : Controller
-    {
-        // GET: AssessmentTests
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class AssessmentTestsController : Controller
+	{
+		private readonly KEPT_DBEntities _context;
+		private readonly AssessmentsQuestionsRepository _assessment;
+		public AssessmentTestsController()
+		{
+			_context = new KEPT_DBEntities();
+			_assessment = new AssessmentsQuestionsRepository(_context);
+		}
+
+		// GET: AssessmentTests
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public JsonResult GetAssessment()
+		{
+			var AssessmentsQuestions = _assessment.GetAssessments();
+			return new JsonResult { Data = AssessmentsQuestions, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+		}
+	}
 }
---
+++ KeptWeb/Repositories/AssessmentsQuestionsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KeptWeb.Models;
+using KeptWeb.ViewModels;
+
+namespace KeptWeb.Repositories
+{
+	public class AssessmentsQuestionsRepository
+	{
+		private readonly KEPT_DBEntities _context;
+		public AssessmentsQuestionsRepository(KEPT_DBEntities context)
+		{
+			_context = context;
+		}
+
+		public IList<AssessmentQuestions> GetAssessments()
+		{
+			var assessmentQuestions = new List<AssessmentQuestions>();
+			var competencies = _context.CompetencyAndPerformance.ToList();
+			foreach (var comp in competencies) {
+				var questions = _context.AssessmentTestQuestions.Where(q => q.CAndPId == comp.CAndPId).ToList();
+				var assessmentQuestion = new AssessmentQuestions
+				{
+					CAndPId = comp.CAndPId,
+					Name = comp.Name,
+					Description = comp.Description,
+					IsCompetency = comp.IsCompetency,
+					AssessmentTestsQuestions = questions
+				};
+				assessmentQuestions.Add(assessmentQuestion);
+			}
+			return assessmentQuestions;
+		}
+	}
+}
