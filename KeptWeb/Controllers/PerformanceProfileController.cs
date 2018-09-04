58c8dec87e9c3fef847171b71098c8cb8800691e
Nicolás Vallejo <nivato@msn.com>
2018-09-03 19:44:00 -0500

    returning performance profile model

--- KeptWeb/Controllers/PerformanceProfileController.cs
+++ KeptWeb/Controllers/PerformanceProfileController.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KeptWeb.Models;
+using KeptWeb.Repositories;
+using Microsoft.AspNet.Identity;
 
 namespace KeptWeb.Controllers
 {
     public class PerformanceprofileController : Controller
     {
-        // GET: Performanceprofile
+        private readonly KEPT_DBEntities _context;
+        private readonly EmployeeRepository _employee;
+
+        public PerformanceprofileController()
+        {
+            _context = new KEPT_DBEntities();
+            _employee = new EmployeeRepository(_context);
+        }
+
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var employee = _employee.GetEmployee(User.Identity.GetUserId());
+            var profile = _context.PerformanceProfile.ToList();
+            var competencies = _context.CompetencyAndPerformance.Where(cp => cp.IsCompetency == true).ToList();
+            var perrformance = _context.CompetencyAndPerformance.Where(cp => cp.IsCompetency == false).ToList();
+            var viewmodel = new PerformanceProfileModel { Employee = employee, Profile = null, Competencies = null, Performance = null };
+            return View(viewmodel);
         }
     }
 }
